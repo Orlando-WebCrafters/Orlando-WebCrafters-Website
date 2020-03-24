8d89b1e6332c27e3db35f4a11551cb250a4b3e88
Minh Le <minhle30964@gmail.com>
2020-03-24 11:55:47 -0400

    test for contact page

---
+++ contact.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: minhl
+ * Date: 3/24/2020
+ * Time: 11:43 AM
+ */
+    if(isset($_POST['submit'])){
+        $name=$_POST['name'];
+        $email=$_POST['email'];
+        $msg=$_POST['msg'];
+
+        $to='webvineorganization@gmail.com'; // Receiver Email ID, Replace with your email ID
+        $subject='WebVine Form Submission';
+        $message="Name :".$name."\n"."Wrote the following :"."\n\n".$msg;
+        $headers="From: ".$email;
+
+        if(mail($to, $subject, $message, $headers)){
+            echo "<h1>Sent Successfully! Thank you"." ".$name.", We will contact you shortly!</h1>";
+        }
+        else{
+            echo "Something went wrong!";
+        }
+    }
+?>
